3fd4a993190e85650de9aa2c0d084ca3aa6da5e5
Kirill <uspenskiyke@mail.ru>
2022-04-11 13:26:38 +0400

    OOP_Part2_Домашняя работа.

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,2 +1,35 @@
-package com.company;public class Car {
+package com.company;
+
+public class Car extends Vehicle{
+
+
+    public Car(String modelName, int wheelsCount) {
+       super(modelName, wheelsCount);
+    }
+
+    public String getModelName() {
+        return super.getModelName();
+    }
+
+    public int getWheelsCount() {
+        return super.getWheelsCount();
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        super.setWheelsCount(wheelsCount);
+    }
+
+    public void setModelName(String modelName) {
+        super.setModelName(modelName);
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+
 }
