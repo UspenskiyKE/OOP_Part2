d8c38d659ef01bf185bae27dd9c75d6bf3206c62
Kirill <uspenskiyke@mail.ru>
2022-04-11 23:43:09 +0400

    OOP_Part2_Домашняя работа. Исправление.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,11 +15,11 @@ public class Main {
 
 
         ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
+        station.checkCar(car);
+        station.checkCar(car2);
+        station.checkTruck(truck);
+        station.checkTruck(truck2);
+        station.checkBicycle(bicycle);
+        station.checkBicycle(bicycle2);
     }
 }
